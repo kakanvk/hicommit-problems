cc6f0492937c529e16cfd03433c94e1813fcd678
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-08-14 17:31:52 +0700

    Submit code at 18:30:56 14/8/2024 by HiCommit

--- Main.java
+++ Main.java
@@ -1,13 +1,8 @@
-// HICOMMIT TEMPLATE FOR JAVA
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*
-            Your code goes here. Happy coding!
-        */
 
     }
 }
