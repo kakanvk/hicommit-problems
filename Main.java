abf3854450edb782bbba83e84c30412d055cd0ec
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-08-18 19:39:55 +0700

    Submit code at 20:38:44 18/8/2024 by HiCommit

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ public class Main {
         if (n < 2) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
+        for (int i = 2; i <= Math.sqrt(n)/0; i++) {
             if (n % i == 0) {
                 return false;
             }
