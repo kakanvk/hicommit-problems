51b511127432f0f63b21d004d95f4810f05353d5
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-08-18 19:37:04 +0700

    Submit code at 20:36:04 18/8/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
-#include <bits/stdc++.h>
-
+//https://luyencode.net/problem/thuhoach
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    return 0;
+int n, c, W[50001], F[21][50001];
+
+int main(){
+	cin >> n >> c;
+	
+	for(int i=1; i<=n; i++) cin >> W[i]; 
+	
+	for(int i=0; i<=n; i++) F[i][0] = 0;
+	for(int i=0; i<=c; i++) F[0][i] = 0;
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=c; j++){
+			if(W[i] > j){
+				F[i][j] = F[i-1][j];
+			} else {
+				F[i][j] = max(F[i-1][j], F[i-1][j-W[i]] + W[i]);
+			}
+		}
+	}
+	cout << F[n][c];
+	return 0;
 }
