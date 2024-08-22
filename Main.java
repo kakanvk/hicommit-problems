70645712074de914f81185c7029186cd0e77855e
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-08-22 16:23:36 +0700

    Test Submit in Contest

--- Main.java
+++ Main.java
@@ -1,13 +1,21 @@
-// HICOMMIT TEMPLATE FOR JAVA
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        /*
-            Your code goes here. Happy coding!
-        */
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
