3bc983472cb4e533cfc2ed635da65c4f3c1b6da6
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-08-23 17:11:21 +0700

    Submit code at 18:10:09 23/8/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -5,6 +5,17 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+
+    long long n;
+    cin >> n;
+
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+
+    cout << sum << endl;
+
     return 0;
 }
