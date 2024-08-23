7e7f8db39958cee9d067581d379a8697fc41278c
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-08-23 17:25:01 +0700

    Submit code at 18:23:16 23/8/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
-// HICOMMIT TEMPLATE FOR C++
 #include <bits/stdc++.h>
 
 using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) return false;
+    }
+    return true;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    /*
-        Your code goes here. Happy coding!
-    */
+    int n;
+    cin >> n;
+
+    if (isPrime(n)) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
 
     return 0;
 }
