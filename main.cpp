7f2c53decef81156df625c4871641684d9cbc9fa
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-08-23 17:30:16 +0700

    Submit code at 18:29:08 23/8/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -5,24 +5,6 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    long long n;
-    cin >> n;
-
-    long long fib[46];
-    fib[0] = 0;
-    fib[1] = 1;
-
-    for (long long i = 2; i <= n; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-
-    long long sum = 0;
-    for (long long i = 0; i <= n; i++) {
-        sum += fib[i];
-    }
-
-    cout << sum << endl;
     
     return 0;
 }
