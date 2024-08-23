40a490a66e222cebb9b945e2848f7dafea634842
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-08-23 17:27:25 +0700

    Submit code at 18:25:15 23/8/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// HICOMMIT TEMPLATE FOR C++
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,9 +6,23 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    /*
-        Your code goes here. Happy coding!
-    */
+    long long n;
+    cin >> n;
 
+    long long fib[46];
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (long long i = 2; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    long long sum = 0;
+    for (long long i = 0; i <= n; i++) {
+        sum += fib[i];
+    }
+
+    cout << sum << endl;
+    
     return 0;
 }
