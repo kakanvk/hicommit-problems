70a874c6474e65267d6c7beed52616016a1a5270
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-10-04 08:46:15 +0700

    Re-submit code at 08:43:58 4/10/2024 by HiCommit

--- main.c
+++ main.c
@@ -4,16 +4,16 @@ int main() {
 
   int temp, n, max;
 
-  scanf("%d%d", &n, &max);
+  scanf ("%d%d", &n, &max);
 
   n--;
   
-  while(n--){
-    scanf("%d", &temp);
+  while(n--) {
+    scanf ("%d", &temp);
     if(temp > max) max = temp;
   }
 
-  printf("%d", max);
+  printf ("%d", max);
   
   return 0;
 }
