511cc8d8aa33f5c0b2422b20897efd4750eb7c13
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-10-05 21:29:22 +0700

    Submit code at 21:25:44 5/10/2024 by HiCommit

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main() {
+
+  int n, sumOfEven = 0;
+
+  scanf("%d", &n);
+
+  for(i=1; i<=n; i++){
+    sumOfEven += i%2==0 ? i : 0;
+  }
+  
+  printf("%d", sumOfEven);
+  
   return 0;
 }
