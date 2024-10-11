92c769785d76ba02466596fffd8f10112925556b
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-10-11 21:58:30 +0700

    Submit code at 21:56:15 11/10/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
-// HICOMMIT TEMPLATE FOR C++
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
+long long sumOfPrimeDivisors(int N) {
+    long long sum = 0;
+    vector<bool> isPrime(N + 1, true);
+    
+    for (int i = 2; i <= N; i++) {
+        if (isPrime[i]) {
+
+            if (N % i == 0) {
+                sum += i;
+            }
+
+            for (int j = 2 * i; j <= N; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    return sum;
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    /*
-        Your code goes here. Happy coding!
-    */
-
+    int N;
+    cin >> N;
+    cout << sumOfPrimeDivisors(N) << endl; 
     return 0;
 }
