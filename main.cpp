aeb06f2af2006c721895e91f7947ce7358395400
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-10-11 22:37:16 +0700

    Submit code at 22:35:22 11/10/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
-// HICOMMIT TEMPLATE FOR C++
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int N;
+    cin >> N;
+    vector<int> h(N);
+    for (int i = 0; i < N; i++) {
+        cin >> h[i];
+    }
+    
+    vector<int> dp(N, 0);
+    dp[1] = abs(h[1] - h[0]);
 
-    /*
-        Your code goes here. Happy coding!
-    */
+    for (int i = 2; i < N; i++) {
+        dp[i] = min(dp[i-1] + abs(h[i] - h[i-1]), dp[i-2] + abs(h[i] - h[i-2]));
+    }
 
+    cout << dp[N-1] << endl;
     return 0;
 }
