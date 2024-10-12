5451877a2817e774fe7becee551b7e6da384d277
kakanvk <caoka5265@gmail.com>
2024-10-12 16:08:33 +0700

    Submit Problem

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
-// HICOMMIT TEMPLATE FOR C++
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
+long long fibonacci(int n) {
+    if (n <= 1) return n;
+    
+    long long prev2 = 0, prev1 = 1, current;
+    
+    for (int i = 2; i <= n; i++) {
+        current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+    
+    return current;
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    /*
-        Your code goes here. Happy coding!
-    */
-
+    int n;
+    cin >> n;
+    cout << fibonacci(n) << endl;
     return 0;
 }
