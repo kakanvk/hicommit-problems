5175909c5f4b84900e1bd893c962f0f1fbd750b0
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2024-10-28 14:17:00 +0700

    Submit code at 14:14:21 28/10/2024 by HiCommit error

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-// HICOMMIT TEMPLATE FOR C++
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-    /*
-        Your code goes here. Happy coding!
-    */
+  int a, b;
 
-    return 0;
+  cin >> a >> b;
+  cout << A + b;
+  
+    
+  return 0;
 }
